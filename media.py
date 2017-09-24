b93ab17b11150ad8c39008b3e7bbb62f7548ad7a
MuhammadSaeedBatikh <mohammud.saeed.batekh@gmail.co>
2017-09-24 02:52:42 +0200

    comments added documentation for classes and __init__ breaking strings

--- media.py
+++ media.py
@@ -1,4 +1,7 @@
 class VisualMedia():
+    """this class is a super class that holds common 
+    fields/functionality of Movies, TVSeries or whatever to be added later and considered to
+    be some sort of a Visual media"""
 
     def __init__(self, title, story_line, youtube_trailer_url,
                  poster_image_url, genre):
@@ -10,10 +13,17 @@ class VisualMedia():
 
 
 class Movie(VisualMedia):
+    """This is a Movie Class that contains some attributes specific to movies
+     such as duration time, rating, etc.
+     it also inherits VisualMedia 
+     """
     RATINGS = ['G', 'PG', 'PG-13', 'R', 'UNRATED']
 
     def __init__(self, title, story_line, youtube_trailer_url, poster_image_url,
                  release_date, genre, duration_time, rating=RATINGS[3]):
+        """instantiate a Movie object with these attributes: title, story_line, youtube_trailer_url, poster_image_url,
+                 release_date, genre, duration_time, rating"""
+
         VisualMedia.__init__(self, title, story_line, youtube_trailer_url, poster_image_url, genre)
         self.release_date = release_date
         self.duration_time = duration_time
@@ -21,10 +31,17 @@ class Movie(VisualMedia):
 
 
 class TVSeries(VisualMedia):
+    """ This is a TVSeries Class that contains some attributes specific to TV Series
+        such as running date , number of seasons, etc.
+        it also inherits VisualMedia 
+        """
     RATINGS = ['TV-Y', 'TV-Y7', 'TV-G', 'TV-PG', 'TV-14', 'TV-MA']
 
     def __init__(self, title, story_line, youtube_trailer_url, poster_image_url,
                  running_date, genre, seasons_number, rating=RATINGS[4]):
+        """instantiate a Movie object with these attributes: title, story_line, youtube_trailer_url, poster_image_url,
+                 running_date, genre, seasons_number, rating"""
+
         VisualMedia.__init__(self, title, story_line, youtube_trailer_url, poster_image_url, genre)
 
         self.running_date = running_date
